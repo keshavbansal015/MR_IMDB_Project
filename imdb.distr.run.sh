81caa48ec0a1a15cf37dff7287cbbc3e9a5e204a
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2023-12-09 17:54:52 -0600

    correct build files updated

--- imdb.distr.run.sh
+++ imdb.distr.run.sh
@@ -5,11 +5,14 @@
 #SBATCH --output="imdb.distr.out"
 #SBATCH --partition=compute
 ## allocate 3 nodes for the Hadoop cluster: 3 datanodes, from which 1 is namenode
-#SBATCH --nodes=1
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=5G
 #SBATCH --export=ALL
 #SBATCH --time=10
+
+rm ~/.ssh/known_hosts
+export HADOOP_CONFG_DIR=/home/$USER/expanse
 
 module reset
 module unload cpu/0.17.3b 
@@ -19,16 +22,15 @@ module load openjdk
 
 SW=/expanse/lustre/projects/uot191/fegaras 
 export HADOOP_HOME-$SW/hadoop-3.2.2
-PATH-"$HADOOP_HOME/bin:$PATH"
+export MYHADOOP_HOME-$SW/myhadoop
+PATH-"$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$MYHAOOP_HOME/bin:$PATH"
 
-rm -rf imdb.jar
-rm -rf classes
-mkdir -p classes
+myhadoop-configure.sh -s /scratch/$USER/job_$SLURM_JOBID
 
+start-all.sh
 
-javac -d classes -cp classes:'hadoop classpath' src/*.java
-jar of imdb.jar -C classes .
-
-chmod -R 750 classes
-chmod -R 750 imdb.jar
-echo "Compilation completed."
+hdfs dfs -rm -r /user/$USER/*
+hdfs dfs -mkdir -p /user/$USER
+hdfs dfs -mkdir -p /user/vsabhaya
+hdfs dfs -mkdir -p /user/$USER/input
+echo "Directory created"
